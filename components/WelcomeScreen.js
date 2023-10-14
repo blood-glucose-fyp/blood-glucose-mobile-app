621ba21cc0cdf16cec219d5aba57d7812aba2bce
Sean Marinas <seanmarinas@gmail.com>
2023-10-14 23:06:04 +0800

    Changes

---
+++ components/WelcomeScreen.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const WelcomeScreen = () => {};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({});
